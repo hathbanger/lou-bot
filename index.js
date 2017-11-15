5dc1505490785a9d195cb85638bd23b73f46021e
Hathaway <ahatha200@comcast.com>
2017-11-15 09:09:26 -0700

    remove erroneous space

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.LOU_PORT });
@@ -8,41 +7,33 @@ const apiAiService = apiai(apiAiAccessToken);
 const express = require('express')
 const app = express()
 var cors = require('cors');
+const url = require('url');
+
 app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const http = require('http');
-const url = require('url');
-
-
 function isDefined(obj) {
     if (typeof obj == 'undefined') {
         return false;
     }
-
     if (!obj) {
         return false;
     }
-
     return obj != null;
 }  
 
 wss.on('connection', function connection(ws, req) {
-  const location = url.parse(req.url, true);
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    const location = url.parse(req.url, true);
+    ws.on('message', function incoming(message) {
     let request = apiAiService.textRequest(message,
         {
             sessionId: 'test',
             contexts: [
                 {
                     name: "generic",
-                    parameters: {
-                        slack_user_id: 'userId',
-                        slack_channel: 'channel'
-                    }
+                    parameters: {}
                 }
             ]
         });
@@ -52,7 +43,6 @@ wss.on('connection', function connection(ws, req) {
             if (isDefined(response.result)) {
                 let responseText = response.result.fulfillment.speech;
                 let responseData = response.result.fulfillment.data;
-
                 if (isDefined(responseData) && isDefined(responseData.slack)) {
                     console.log(message, response.result.fulfillment);
                 } else if (isDefined(responseText)) {
@@ -66,6 +56,4 @@ wss.on('connection', function connection(ws, req) {
         request.on('error', (error) => console.error(error));
         request.end();         
   });
-
 });
-
